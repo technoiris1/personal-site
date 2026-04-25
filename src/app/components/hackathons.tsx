d8391cf839ff86bbae2d8008e566defc982334fe
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-04-25 14:21:56 +0530

    made dummy cards for all the sections

---
+++ src/app/components/hackathons.tsx
@@ -0,0 +1,10 @@
+export default function Hackathons(){
+    return(
+    <div className="bg-[#fffdf6] w-125 h-125 border-[3px] border-[#201d15] flex flex-col">
+    <div className="pt-8 pl-8">
+    <p className="hero-name text-5xl tracking-normal text-[#201d15]">Hi, I'm Manan.</p>
+    </div>
+    <div className="flex-1 pt-4 pl-8">
+        <p className="text-[#201d15]" style={{ fontFamily: 'var(--font-montserrat)', fontWeight: '550' }}>I'm 16 and I like machines.</p>
+        </div>
+    </div>)}
