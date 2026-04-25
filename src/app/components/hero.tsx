9524a69ff61acd7a0757b4a639496c1ce8a594f8
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-04-25 13:33:27 +0530

    made a basic card system

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,5 @@
+export default function Hero(){
+    return(
+    <div className="bg-[#fffdf6] w-[500px] h-[500px] border border-[#201d15] border-[3px]">
+    <p className="hero-name text-5xl tracking-normal text-[#201d15]">Hi, I'm Manan</p>
+    </div>)}
