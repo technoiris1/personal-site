0a1e3e4bca9786f3e67e31c7b283e0690936d476
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-04-25 15:41:53 +0530

    mmmmm

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -4,9 +4,9 @@ type HeroProps = {
 
 export default function Hero({ className = '' }: HeroProps){
     return(
-    <div className={`bg-[#fffdf6] w-full h-full border-[3px] border-[#201d15] flex flex-col ${className}`}>
+    <div className={`bg-[#fffdf6] w-full h-full flex flex-col ${className}`}>
     <div className="pt-8 pl-8">
-    <p className="hero-name text-5xl tracking-normal text-[#201d15]">Hi, I'm Manan.</p>
+    <p className="hero-name text-7xl tracking-normal text-[#201d15]">Hi, I'm Manan.</p>
     </div>
     <div className="flex-1 pt-4 pl-8">
         <p className="text-[#201d15]" style={{ fontFamily: 'var(--font-montserrat)', fontWeight: '550' }}>I'm 16 and I like machines.</p>
