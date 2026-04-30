6dd9a6b6d597cd08953c539a6d9a6cc8d3e6bf55
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-05-01 04:07:58 +0530

    yeah

--- src/app/components/hackathons.tsx
+++ src/app/components/hackathons.tsx
@@ -6,9 +6,9 @@ export default function Hackathons({ className = '' }: HackathonsProps){
     return(
     <div className={`bg-[#fffdf6] w-full h-full border-[3px] border-[#201d15] flex flex-col ${className}`}>
     <div className="pt-8 pl-8">
-    <p className="hero-name text-5xl tracking-normal text-[#201d15]">Hackathons</p>
+    <p className="hero-name text-5xl tracking-normal text-[#201d15]">TBD</p>
     </div>
     <div className="flex-1 pt-4 pl-8">
-        <p className="text-[#201d15]" style={{ fontFamily: 'var(--font-montserrat)', fontWeight: '550' }}>I'm 16 and I like machines.</p>
+        <p className="text-[#201d15]" style={{ fontFamily: 'var(--font-montserrat)', fontWeight: '550' }}>lalalalalalalalalalalalalalalalalalaalalalalalall</p>
         </div>
     </div>)}
