5dd0540904d746cf3c6c70406a5af9195da059c0
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-05-01 03:32:40 +0530

    trying to make it responsive: starting with small screens, it has a grid like benito board structure for big screens but now it will stack the cards one above each other for screens like tablets and phones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import Blog from "./components/blog";
 import Photography from "./components/photography";
 export default function Home() {
   return (
-    <div className="bg-[#fffdf6] h-screen w-screen overflow-hidden p-4">
+    <div className="bg-[#fffdf6] h-screen w-screen overflow-y-auto lg:overflow-hidden p-4">
 
-      <div className="flex h-full w-full gap-4 min-w-0">
-        <div className="flex w-[42%] min-w-0 flex-col gap-4">
+      <div className="flex flex-col lg:flex-row h-full w-full gap-4 min-w-0">
+        <div className="flex w-full lg:w-[42%] min-w-0 flex-col gap-4">
           <div className="flex-[0.9] min-h-0">
             <Hero />
           </div>
@@ -17,14 +17,14 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="grid flex-1 min-w-0 grid-cols-2 grid-rows-2 gap-4">
+        <div className="grid flex-1 min-w-0 grid-cols-1 lg:grid-cols-2 lg:grid-rows-2 gap-4">
           <div className="min-h-0">
             <Projects />
           </div>
           <div className="min-h-0">
             <Hackathons />
           </div>
-          <div className="col-span-2 row-span-2 min-h-0">
+          <div className="col-span-1 lg:col-span-2 lg:row-span-2 min-h-0">
             <Photography />
           </div>
         </div>
