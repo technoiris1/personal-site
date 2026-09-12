0553c7e28f4f6627d6f52a696d616e9fb660835b
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-09-12 17:21:29 +0530

    fixed the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,14 @@ export default function Home() {
         <div className="frame-content">
           <section className="intro-copy" aria-labelledby="intro-title">
             <h1 id="intro-title">Heyo!<br /><em>I&apos;m Manan</em></h1>
-            <p className="pronunciation">//muh-nun//</p>
+            <p className="pronunciation">{"//muh-nun//"}</p>
             <div className="bio-copy">
-              <p>I&apos;m a 17 y/o from India. I like to make webapps and hardware. One more thing I really REALLY love are hackathons. I have attended and organised a bunch. I got into computers when I was in 6th grade, my school had a workshop on how to make games with scratch.</p>
-              <p>I&apos;m active in a few communities, like Hack Club. If you&apos;re a teen, JOIN HACK CLUB: <Link href="https://hackclub.com">hackclub.com.</Link></p>
+              <p>I&apos;m a 17 y/o from India. I like to make webapps and hardware. One thing I really REALLY love are hackathons. I have attended and organised a bunch, and they've been a huge part of my life. </p>
+                <p>I got into computers when I was in 6th grade, my school hosted a workshop on how to make games with scratch. Since then I've been trying to learn more about machines. </p>
+                <p>These days, I help run events @ <Link href="https://hackclub.com" target="_blank">Hack Club </Link> and work as an Operations Contributor @ <Link href="https://hackclub.com/fiscal-sponsorship" target="_blank"> HCB.</Link> I&apos;m active in a few communities like Hack Club. It has introduced me to some of the most incredible people I know. </p>
+                
+                <p>
+                If you&apos;re a teen, JOIN HACK CLUB. <Link href="https://hackclub.com">hackclub.com.</Link> </p>
             </div>
           </section>
         </div>
