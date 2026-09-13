0312f4f8b717d2c07efe5f1a51b90ee1ce4a17d5
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-09-13 13:00:37 +0530

    made it responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             <p className="pronunciation">{"//muh-nun//"}</p>
             <div className="bio-copy">
               <p>I&apos;m a 17 y/o from India. I like to make webapps and hardware. One thing I really REALLY love are hackathons. I have attended and organised a bunch, and they&apos;ve been a huge part of my life. </p>
-                <p>I got into computers when I was in 6th grade, my school hosted a workshop on how to make games with scratch. Since then I&apos;ve been trying to learn more about machines. </p>
+                <p>I got into computers when I was in 6th grade. Since then I&apos;ve been trying to learn more about machines. </p>
                 <p>These days, I help run events @ <Link href="https://hackclub.com" target="_blank">Hack Club </Link> and work as an Operations Contributor @ <Link href="https://hackclub.com/fiscal-sponsorship" target="_blank"> HCB.</Link> I&apos;m active in a few communities like Hack Club. It has introduced me to some of the most incredible people I know. </p>
                 
                 <p>
