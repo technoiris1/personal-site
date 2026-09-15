116c5e08808467b153e398a370102922cf3b23fb
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-09-15 20:31:12 +0530

    ich bin ein heidelberger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,14 @@ export default function Home() {
         <div className="frame-content">
           <section className="intro-copy" aria-labelledby="intro-title">
             <h1 id="intro-title">Heyo!<br /><em>I&apos;m Manan</em></h1>
-            <p className="pronunciation">{"//muh-nun//"}</p>
+            <p className="pronunciation font-[#8a1c0c]">{"//muh-nun//"}</p>
             <div className="bio-copy">
-              <p>I&apos;m a 17 y/o from India. I like to make webapps and hardware. One thing I really REALLY love are hackathons. I have attended and organised a bunch, and they&apos;ve been a huge part of my life. </p>
-                <p>I got into computers when I was in 6th grade. Since then I&apos;ve been trying to learn more about machines. </p>
-                <p>These days, I help run events @ <Link href="https://hackclub.com" target="_blank">Hack Club </Link> and work as an Operations Contributor @ <Link href="https://hackclub.com/fiscal-sponsorship" target="_blank"> HCB.</Link> I&apos;m active in a few communities like Hack Club. It has introduced me to some of the most incredible people I know. </p>
+              <p>ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger</p>
+                <p>ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger </p>
+                <p>ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger ich bin ein heidelberger </p>
                 
                 <p>
-                If you&apos;re a teen, JOIN HACK CLUB. <Link href="https://hackclub.com">hackclub.com.</Link> </p>
+                If you&apos;re a teen, JOIN HACK CLUB. <Link href="https://hackclub.com" className="text-[#8a1c0c] hover:underline">hackclub.com.</Link> </p>
             </div>
             <nav className="social-links" aria-label="Social links">
               <Link href="https://github.com/technoiris1" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
